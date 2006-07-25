a69f94868b1aec5e9de482e906a28b1d7f0fb3f3
Aaron C. de Bruyn <aaron@heyaaron.com>
2006-07-25 05:13:01 +0000

    Starting python rewrite. Starting gtk rewrite.
    git-svn-id: svn://localhost/snppsend@8 fb70afd9-5408-0410-86be-db8619026670

--- trunk/s2.py
+++ trunk/s2.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python
-
-import asyncore
 import socket
 
-class snpp_client(asyncore.dispatcher):
-	def __init__(self, host, port):
-		asyncore.dispatcher.__init__(self)
-		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.connect((host, port))
-	
-	def handle_connect(self):
-		pass
+class snppsocket:
+	def __init__(self,sock=None):
+		if sock is None:
+			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.sock.settimeout(5)
+		else:
+			self.sock = sock
 
-	def handle_read(self):
-		data = self.recv(8192)
-		print data
-	
-	def writable(self):
-		return (len(self.buffer) > 0)
+	def __chat(self,dataout,response):
+		if dataout:
+			self.sock.send(dataout)
+		datain = ''
+		while 1:
+			try:
+				chunk = self.sock.recv(1)
+				if chunk == '':
+					break
+			except:
+				break
+			datain = datain + chunk
+		print "Chat Data: [%s]" %(datain)
+		print "Looking: [%s]" %(datain[0:3])
+		if str(datain[0:3]) == str(response): return True
 
-	def handle_write(self):
-		sent = self.send(self.buffer)
-		self.buffer = self.buffer[sent:]
 
-sk = socket.socket()
-sc = snpp_client(sk, "67.115.154.70", "444")
+	def sendmessage(self,host,port,message):
+		self.sock.connect((host, port))
+
+
+		#if not self.__chat(None, 220): print "Connection returned failure"
+		#if not self.__chat("PAGE 3606901574\n", 250) == None: print "Pager number not accepted"
+
+		return "Sent..."
+
+s = snppsocket()
+print s.sendmessage("67.115.154.70",444,"test message")
+
 
 
 #connect, 220, "PAGE nnnnnnnnnn", 250, "MESS message", 250, "SEND", 250
