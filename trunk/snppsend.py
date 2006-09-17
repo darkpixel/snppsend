2c1cc31d58e357506ebddd8bc47e44b48b052e8e
Aaron C. de Bruyn <aaron@heyaaron.com>
2006-09-17 06:32:15 +0000

    Socket code converted to python.  Almost friggin ready...
    git-svn-id: svn://localhost/snppsend@9 fb70afd9-5408-0410-86be-db8619026670

--- trunk/snppsend.py
+++ trunk/snppsend.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 # snppsend v2.00a: a simple program to deliver messages to text pagers
-#                 using the SNPP protocol.
-# Copyright (C) 2004 - 2005 Aaron C. de Bruyn <code@darkpixel.com>
+#                  using the SNPP protocol.
+# Copyright (C) 2004 - 2006 Aaron C. de Bruyn <code@darkpixel.com>
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -21,22 +21,105 @@
 # The latest version of snppsend should always be available
 # on my website at http://www.darkpixel.com/
 # 
-# Some shout-outs
+# Special thanks to...
 # darklordmaven for hammering away at my scripts, looking
 #   for bugs, moral support, and games of Halo 2.
-# jeek in #pound-perl.pm on EFNet for his words of wisdom.
-#   Though he spoke not more than ten words, he pointed
-#   out my total perl noobness.
-# Steve Kaylor and Brandon Zehm - their original script
-#   inspired this program and I borrowed a chunk of
-#   their socket and snpp server communication code for
-#   the original version of snppsend which was written
-#   in perl.
+# jeek in #pound-perl.pm on EFNet for pointing out my perl noobness
+# Steve Kaylor and Brandon Zehm - their original script that inspired this app
 
-import sqlite
+import asynchat, asyncore, socket, sys, ConfigParser
+from asynchat import async_chat
 
-conn = sqlite.connect("snppsend.db")
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM PROVIDERS;")
-print cursor.fetchall()
+
+def debug(*args):
+    print "debug: ", " ".join(args)
+
+class SNPPChannel:
+	def __init__(self):
+		self.data= []
+		self.state = 0
+
+	def get_data(self):
+		tmp = "".join(self.data)
+		del self.data[:]
+		return tmp
+
+	def collect(self,data):
+		self.data.append(data)
+
+	def set_state(self,newstate):
+		self.state = newstate
+		# 0 - Initialized
+		# 1 - Connected (Send Pager ID)
+		# 2 - Pager Accepted (Send message)
+		# 3 - Message Accepted (Quit)
+		# 4 = Message Sent
+		# 5 = Quitting
+
+	def get_state(self):
+		return self.state
+	
+
+class SNPP(async_chat):
+	def __init__(self,addr, pager, message):
+		async_chat.__init__(self)
+		self.remote = addr
+		self.create_socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.connect(addr)
+		self.pager = pager
+		self.message = message
+	
+		self.chan = SNPPChannel()
+		self.collect_incoming_data = self.chan.collect
+
+		async_chat.set_terminator(self,"\r\n")
+
+	def send_command(self,command):
+		debug("send_command: " + command)
+		self.push(command + "\r\n")
+
+	def found_terminator(self):
+		tmp = self.chan.get_data()
+		debug("found_terminator: " + tmp)
+
+		if tmp.startswith("220"):
+			debug("IT STARTS WITH 220!  WOOT!")
+
+		debug(str(self.chan.get_state()))
+		if self.chan.get_state() == 1:
+			self.send_command("PAGE " + self.pager)
+			self.chan.set_state(2)
+		elif self.chan.get_state() == 2:
+			self.send_command("MESS Test message...")
+			self.chan.set_state(3)
+		elif self.chan.get_state() == 3:
+			self.send_command("SEND")
+			self.chan.set_state(4)
+		elif self.chan.get_state() == 4:
+			self.send_command("QUIT")
+		else:
+			debug("Unknown SNPP state!")
+			sys.exit("Invalid socket state")
+
+	def handle_connect(self):
+		debug("socket connected")
+		self.chan.set_state(1)
+	
+	def handle_close(self):
+		debug("socket closed")
+		self.close()
+		self.chan.set_state(0)
+	
+
+
+#cfg = RawConfigParser
+#cfg.add_section("Providers")
+
+
+client = SNPP(("67.115.154.70", 444), "3600000000", "Test message...")
+
+try:
+	asyncore.loop()
+except KeyboardInterrupt:
+	debug("Windows would probably just coredump here...")
 
